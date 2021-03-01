fad427aac1158c5816f0bea868ef736d7b5a6c42
Sergey Sudakov <s3gaddtv@gmail.com>
2021-03-01 15:17:18 +0300

    feat: create sky overlay

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import StarOverlay from '../components/StarOverlay/StarOverlay';
+
+const App: React.FC = () => (
+    <>
+        <StarOverlay />
+    </>
+);
+
+export default App;
---
+++ src/components/StarOverlay/StarOverlay.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './StarOverlay.scss';
+
+const StarOverlay: React.FC = () => (
+    <div className="star-overlay">
+        <div className="star-overlay__item" />
+        <div className="star-overlay__item" />
+        <div className="star-overlay__item" />
+    </div>
+);
+
+export default StarOverlay;
